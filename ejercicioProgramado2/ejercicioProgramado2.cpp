a8b5c5926c83bd2532fec6c52c9bb8136a8b7c0c
javier25arroyo <jpereza2@ucenfotec.ac.cr>
2024-07-11 21:54:00 -0600

    Add project files.

---
+++ ejercicioProgramado2/ejercicioProgramado2.cpp
@@ -0,0 +1,21 @@
+/*Se dispone de una colección de 10 CDs de los cuales se guarda la siguiente información: a. Nombre del grupo o intérprete b. Fecha de edición c. Firma discográfica d. Duración del CD Se quiere implementar un programa que ordene la colección por nombre de grupo o intérprete teniendo en cuenta que ante dos CDs del mismo grupo estos deben aparecer ordenados por fecha de edición. El programa debe realizar la ordenación lo más rápidamente posible. Justifique las decisiones adoptadas. Tenga en cuenta que el nombre de algunos intérpretes puede no estar formado por caracteres ASCII, cabe la posibilidad de que la comparación de cadenas de los nombres de grupo o intérprete sea costosa. A efectos de la implementación que se debe realizar supóngase que todos los nombres de intérprete se pueden comparar mediante operadores relacionales, aunque el diseño del algoritmo contemple las restricciones del enunciado.*/
+
+#pragma once
+
+#ifndef __METODOS__
+#define __METODOS__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+#endif // !__METODOS__
