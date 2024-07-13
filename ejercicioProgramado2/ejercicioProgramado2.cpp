b655ae689ffccbb574f87f815ef63471da70560f
javier25arroyo <jpereza2@ucenfotec.ac.cr>
2024-07-12 20:28:55 -0600

    FinalCommit

--- ejercicioProgramado2/ejercicioProgramado2.cpp
+++ ejercicioProgramado2/ejercicioProgramado2.cpp
@@ -1,21 +1,85 @@
-/*Se dispone de una colección de 10 CDs de los cuales se guarda la siguiente información: a. Nombre del grupo o intérprete b. Fecha de edición c. Firma discográfica d. Duración del CD Se quiere implementar un programa que ordene la colección por nombre de grupo o intérprete teniendo en cuenta que ante dos CDs del mismo grupo estos deben aparecer ordenados por fecha de edición. El programa debe realizar la ordenación lo más rápidamente posible. Justifique las decisiones adoptadas. Tenga en cuenta que el nombre de algunos intérpretes puede no estar formado por caracteres ASCII, cabe la posibilidad de que la comparación de cadenas de los nombres de grupo o intérprete sea costosa. A efectos de la implementación que se debe realizar supóngase que todos los nombres de intérprete se pueden comparar mediante operadores relacionales, aunque el diseño del algoritmo contemple las restricciones del enunciado.*/
-
 #pragma once
 
 #ifndef __METODOS__
 #define __METODOS__
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class CD {
+public:
+    string nombreGrupo;
+    string fechaEdicion;
+    string firmaDiscografica;
+    int duracionCD;
+
+    CD(string nombreGrupo, string fechaEdicion, string firmaDiscografica, int duracionCD)
+        : nombreGrupo(nombreGrupo), fechaEdicion(fechaEdicion), firmaDiscografica(firmaDiscografica), duracionCD(duracionCD) {}
+
+    bool operator<(const CD& otroCD) const {
+        if (this->nombreGrupo == otroCD.nombreGrupo) {
+            return this->fechaEdicion < otroCD.fechaEdicion;
+        }
+        return this->nombreGrupo < otroCD.nombreGrupo;
+    }
+
+    void mostrar() const {
+        cout << "Nombre del grupo o interprete: " << nombreGrupo << endl;
+        cout << "Fecha de edicion: " << fechaEdicion << endl;
+        cout << "Firma discografica: " << firmaDiscografica << endl;
+        cout << "Duracion del CD: " << duracionCD << " minutos" << endl;
+    }
+};
+
+void ingresarDatosCD(vector<CD>& coleccion) {
+    string nombreGrupo, fechaEdicion, firmaDiscografica;
+    int duracionCD;
+    int cantidadCDs; // Variable para almacenar la cantidad de CDs a ingresar
+
+    cout << "Ingrese la cantidad de CDs a ingresar: ";
+    cin >> cantidadCDs;
+    cin.ignore(); // Limpiar el buffer de entrada
+
+    for (int i = 0; i < cantidadCDs; ++i) {
+        cout << "Ingrese los datos del CD: " << i + 1 << ":\n";
+        cout << "\nNombre del grupo o interprete: ";
+        getline(cin, nombreGrupo);
+        cout << "\nFecha de edicion (YYYY-MM-DD): ";
+        getline(cin, fechaEdicion);
+        cout << "\nFirma discografica: ";
+        getline(cin, firmaDiscografica);
+        cout << "\nDuracion del CD (en minutos): ";
+        cin >> duracionCD;
+        cout << "\n ";
+        cout << "------------------------------ \n";
+        cin.ignore();  // Para limpiar el buffer de entrada
+
+        coleccion.emplace_back(nombreGrupo, fechaEdicion, firmaDiscografica, duracionCD);
+    }
 }
 
+void mostrarColeccion(const vector<CD>& coleccion) {
+    for (const auto& cd : coleccion) {
+        cd.mostrar();
+        cout << "-----------------------" << endl;
+    }
+}
+
+int main() {
+    vector<CD> coleccion;
+
+    ingresarDatosCD(coleccion);
+
+    sort(coleccion.begin(), coleccion.end());
+
+    cout << "\nColeccion ordenada:\n";
+    mostrarColeccion(coleccion);
+
+    return 0;
+}
 
 #endif // !__METODOS__
